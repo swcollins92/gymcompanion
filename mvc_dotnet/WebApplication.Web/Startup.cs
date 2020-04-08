d4897477a24e65b96698c133b53d099898ffe704
Stephen Collins <swcollins92@gmail.com>
2020-04-08 15:56:56 -0400

    i do not know

--- mvc_dotnet/WebApplication.Web/Startup.cs
+++ mvc_dotnet/WebApplication.Web/Startup.cs
@@ -58,6 +58,8 @@ namespace WebApplication.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -80,5 +82,7 @@ namespace WebApplication.Web
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
-    }
+       
+
+        }
 }
