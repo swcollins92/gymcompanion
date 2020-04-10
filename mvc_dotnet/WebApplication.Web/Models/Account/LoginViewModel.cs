93c8365386aa8d682137b26e2ec17cb108ca2e45
Stephen Collins <swcollins92@gmail.com>
2020-04-10 12:56:44 -0400

    Updated nav bar

--- mvc_dotnet/WebApplication.Web/Models/Account/LoginViewModel.cs
+++ mvc_dotnet/WebApplication.Web/Models/Account/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,10 @@ namespace WebApplication.Web.Models.Account
 {
     public class LoginViewModel
     {
+
         public string Username { get; set; }
 
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
